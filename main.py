486a07b6112fc4efca3285b9d8894dda2e615bed
Chinedu <chineduamadi@protonmail.com>
2020-09-01 14:01:17 +0200

    Successfully read gyro sensor values (sort drift)

--- main.py
+++ main.py
@@ -16,24 +16,33 @@ Infrared sensor:
 Gyrosensor:
 
 """
-infrared_sensor = InfraredSensor(Port.S3)
+#infrared_sensor = InfraredSensor(Port.S3) why aren't you working??
 gyro_sensor = GyroSensor(Port.S2)
-
 
 # Reset gyro [confirm this]
 
+# Read gyro sensor data 
+# Seems to be working if we access the speed and angles individually
 
+for i in range(0,100):
+    # Use panda data frame to write data to csv file to confirm if the angular velocity and robot angle
+# are being outputted
+    print("Angular velocity: {}\n".format(gyro_sensor.speed()))
+
+print("-*-*-*-*-*-*-*-*-*-*-*-*")
+
+for i in range(0,100):
+    print("Robot angle: {}\n".format(gyro_sensor.angle()))
+    
+
+# Do the covariance or variance thing on "excel"
 
 
 # Kalman Filter
-
-
-
+# do we also use motor encoders for this?
 
 
 # PID Controller
-
-
 
 # Go back to motor initialization and confirm if we need more 
 # options to be specified, ie, direction, gear train ratio etc
@@ -56,3 +65,6 @@ if __name__ == "__main__":
     # Define methods for different purposes
     # Main balancing function should be in a loop, how do we initialize?
     # Use try except block then output error
+    
+    
+# Strive to get this project sent to ev3dev. No pressure, but something to strive for, keep pushing
